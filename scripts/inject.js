106f45ef41df87634df6c8d1ccf4cf730ecf1f24
relaxeaza <mafrazzrafael@gmail.com>
2019-05-01 17:09:10 -0300

    Update port of the js injector to match the test server.

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,6 +1,5 @@
-javascript:
-(function(){
-	var s=document.createElement('script');
-	s.src="https://127.0.0.1:8080/tw2overflow.js"
-	document.getElementsByTagName('head')[0].appendChild(s)
+javascript:(function(){
+ var s=document.createElement('script');
+ s.src='https://127.0.0.1:2838/tw2overflow.js';
+ document.getElementsByTagName('head')[0].appendChild(s)
 })()
