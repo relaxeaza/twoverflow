f05d513d0f324b36163d9c321abab533a6946f24
relaxeaza <mafrazzrafael@gmail.com>
2019-05-02 12:40:18 -0300

    Add date.now() to url test script build to avoid cached copys.

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,5 +1,5 @@
 javascript:(function(){
  var s=document.createElement('script');
- s.src='https://127.0.0.1:2838/tw2overflow.js';
+ s.src='https://127.0.0.1:2838/tw2overflow.js?'+Date.now();
  document.getElementsByTagName('head')[0].appendChild(s)
 })()
