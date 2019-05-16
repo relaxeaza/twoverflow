f394c1d3cbfec2ec90a4eba13d3dd5bae1d58458
relaxeaza <mafrazzrafael@gmail.com>
2019-05-16 09:18:02 -0300

    autoCollector: Constant storage keys.

--- src/modules/auto-collector/src/init.js
+++ src/modules/auto-collector/src/init.js
@@ -15,6 +15,10 @@ require([
     Lockr,
     eventQueue
 ) {
+    var STORAGE_KEYS = {
+        ACTIVE: 'auto_collector_active'
+    }
+
     if (autoCollector.isInitialized()) {
         return false
     }
@@ -26,17 +30,17 @@ require([
         autoCollectorInterface()
         
         ready(function () {
-            if (Lockr.get('collector-active', false, true)) {
+            if (Lockr.get(STORAGE_KEYS.ACTIVE, false, true)) {
                 autoCollector.start()
                 autoCollector.secondVillage.start()
             }
 
             eventQueue.register(eventTypeProvider.AUTO_COLLECTOR_STARTED, function () {
-                Lockr.set('collector-active', true)
+                Lockr.set(STORAGE_KEYS.ACTIVE, true)
             })
 
             eventQueue.register(eventTypeProvider.AUTO_COLLECTOR_STOPPED, function () {
-                Lockr.set('collector-active', false)
+                Lockr.set(STORAGE_KEYS.ACTIVE, false)
             })
         }, ['initial_village'])
     })
