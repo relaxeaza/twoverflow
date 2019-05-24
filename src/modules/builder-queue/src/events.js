04c1b21a81769b6557d8355347d6543218448f67
relaxeaza <mafrazzrafael@gmail.com>
2019-05-24 12:15:20 -0300

    builder: Update event values.

--- src/modules/builder-queue/src/events.js
+++ src/modules/builder-queue/src/events.js
@@ -1,14 +1,14 @@
 define('two/builderQueue/events', [], function () {
     angular.extend(eventTypeProvider, {
-        BUILDER_QUEUE_JOB_STARTED: 'Builder/jobStarted',
-        BUILDER_QUEUE_START: 'Builder/start',
-        BUILDER_QUEUE_STOP: 'Builder/stop',
-        BUILDER_QUEUE_UNKNOWN_SETTING: 'Builder/settings/unknownSetting',
-        BUILDER_QUEUE_CLEAR_LOGS: 'Builder/clearLogs',
-        BUILDER_QUEUE_BUILDING_SEQUENCES_UPDATED: 'Builder/buildingOrders/updated',
-        BUILDER_QUEUE_BUILDING_SEQUENCES_ADDED: 'Builder/buildingOrders/added',
-        BUILDER_QUEUE_BUILDING_SEQUENCES_REMOVED: 'Builder/buildingOrders/removed',
-        BUILDER_QUEUE_SETTINGS_CHANGE: 'Builder/settings/change',
+        BUILDER_QUEUE_JOB_STARTED: 'builder_queue_job_started',
+        BUILDER_QUEUE_START: 'builder_queue_start',
+        BUILDER_QUEUE_STOP: 'builder_queue_stop',
+        BUILDER_QUEUE_UNKNOWN_SETTING: 'builder_queue_settings_unknown_setting',
+        BUILDER_QUEUE_CLEAR_LOGS: 'builder_queue_clear_logs',
+        BUILDER_QUEUE_BUILDING_SEQUENCES_UPDATED: 'builder_queue_building_orders_updated',
+        BUILDER_QUEUE_BUILDING_SEQUENCES_ADDED: 'builder_queue_building_orders_added',
+        BUILDER_QUEUE_BUILDING_SEQUENCES_REMOVED: 'builder_queue_building_orders_removed',
+        BUILDER_QUEUE_SETTINGS_CHANGE: 'builder_queue_settings_change',
         BUILDER_QUEUE_NO_SEQUENCES: 'builder_queue_no_sequences'
     })
 })
