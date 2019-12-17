8eab159a8a5f0c6625a70dda3c8185b091c2dc47
relaxeaza <mafrazzrafael@gmail.com>
2019-12-17 01:09:12 -0300

    server script: Moved script to package.json. Run with npm run server.

--- scripts/server.js
+++
@@ -1,8 +0,0 @@
-require('http-server').createServer({
-	root: '../dist/',
-	https: {
-		cert: './https/cert.pem',
-		key: './https/key.pem'
-	}
-}).listen(2838)
-console.log('https://127.0.0.1:2838/')
